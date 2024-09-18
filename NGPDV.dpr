61ce8f7bc7f273ed6b68fc6255dcd016246b22ce
NOT-SUP-LEANDRO <NOT-SUP-LEANDRO@REN-SUP-LEANDRO>
2024-09-18 15:49:08 -0300

    Implementação de Login

--- NGPDV.dpr
+++ NGPDV.dpr
@@ -2,13 +2,16 @@ program NGPDV;
 
 uses
   Vcl.Forms,
-  ngpdv.view.principal in 'src\view\ngpdv.view.principal.pas' {uPrincipal};
+  ngpdv.view.principal in 'src\view\ngpdv.view.principal.pas' {uPrincipal},
+  ngpdv.view.login in 'src\view\ngpdv.view.login.pas' {uLogin};
 
 {$R *.res}
 
 begin
   Application.Initialize;
+  ReportMemoryLeaksOnShutdown := True;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TuPrincipal, uPrincipal);
+  Application.CreateForm(TuLogin, uLogin);
   Application.Run;
 end.
--- src/view/ngpdv.view.principal.pas
+++ src/view/ngpdv.view.principal.pas
@@ -5,7 +5,7 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.Buttons, Data.DB,
-  Vcl.Grids, Vcl.DBGrids;
+  Vcl.Grids, Vcl.DBGrids, Vcl.StdCtrls, Vcl.Imaging.jpeg, ngpdv.view.login;
 
 type
   TuPrincipal = class(TForm)
@@ -34,8 +34,41 @@ type
     shpMaisFuncoes: TShape;
     btnMaisFuncoes: TSpeedButton;
     dbgrdOperacoes: TDBGrid;
+    pnlTotalCompra: TPanel;
+    lblTotalCompras: TLabel;
+    shpTotalCompras: TShape;
+    pnledtTotalCompras: TPanel;
+    lbledtTotalCompras: TLabel;
+    pnlSubTotal: TPanel;
+    lblSubTotal: TLabel;
+    pnledtSubTotal: TPanel;
+    shpSubTotal: TShape;
+    lbledtSubTotal: TLabel;
+    pnlQuantidade: TPanel;
+    lblQuantidade: TLabel;
+    pnledtQuantidade: TPanel;
+    shpQuantidade: TShape;
+    edtQuantidade: TEdit;
+    pnlPreco: TPanel;
+    lblPreco: TLabel;
+    pnledtPreco: TPanel;
+    shpPreco: TShape;
+    lbledtPreco: TLabel;
+    pnlProduto: TPanel;
+    lblProduto: TLabel;
+    pnledtProduto: TPanel;
+    shpProduto: TShape;
+    edtProduto: TEdit;
+    pnlImgProduto: TPanel;
+    imgProdutos: TImage;
+    pnlMaster: TPanel;
+    procedure FormCreate(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
   private
-    { Private declarations }
+    FLogin: TuLogin;
+    procedure MontarBotoes;
+
   public
     { Public declarations }
   end;
@@ -44,6 +77,33 @@ var
   uPrincipal: TuPrincipal;
 
 implementation
+
+procedure TuPrincipal.FormCreate(Sender: TObject);
+begin
+  MontarBotoes;
+end;
+
+procedure TuPrincipal.FormClose(Sender: TObject; var Action: TCloseAction);
+begin
+  FLogin.Free;
+end;
+
+procedure TuPrincipal.FormShow(Sender: TObject);
+begin
+  FLogin  := TuLogin.Create(nil);
+  FLogin.Parent := pnlMaster;
+  FLogin.Show;
+end;
+
+procedure TuPrincipal.MontarBotoes;
+begin
+  btnCancelarOperacao.Caption := 'Cancelar Operação ' + ''#13'' + ' (ESC)';
+  btnConsultarPreco.Caption := 'Consultar Preço ' + ''#13'' + ' (F4)';
+  btnAbrirCaixa.Caption := 'Abrir Caixa ' + ''#13'' + ' (F2)';
+  btnCancelarVenda.Caption := 'Cancelar Venda ' + ''#13'' + ' (F6)';
+  btnCancelarItem.Caption := 'Cancelar Item ' + ''#13'' + ' (F5)';
+  btnMaisFuncoes.Caption := 'Mais FunÇões ' + ''#13'' + ' (F12)';
+end;
 
 {$R *.dfm}
 
